1180e0d988d3293e675ae0385eb7868b8c39ce1a
gcharang <mrgcharang@gmail.com>
2020-09-30 16:36:07 +0530

    discourse tweaks

--- assets/js/scripts/script.js
+++ assets/js/scripts/script.js
@@ -3,7 +3,7 @@
 //*****************
 
 // Event DOM ready
-var callback = function(){
+var callback = function () {
   // trigger events on Dom ready.
 
   // =======
@@ -20,35 +20,35 @@ var callback = function(){
   // Navbar Menu
   // ===========
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
     $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
+      $el.addEventListener("click", function () {
         // Get the target from the "data-target" attribute
         var target = $el.dataset.target;
         var $target = document.getElementById(target);
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
 
-      $el.onkeydown = function(evt) {
+      $el.onkeydown = function (evt) {
         evt = evt || window.event;
         var isEnter = false;
         var isEscape = false;
         if ("key" in evt) {
-            isEnter = (evt.key == "Enter" || evt.key == "Ent");
-            isEscape = (evt.key == "Escape" || evt.key == "Esc");
+          isEnter = evt.key == "Enter" || evt.key == "Ent";
+          isEscape = evt.key == "Escape" || evt.key == "Esc";
         } else {
-            isEnter = (evt.keyCode == 13);
-            isEscape = (evt.keyCode == 27);
+          isEnter = evt.keyCode == 13;
+          isEscape = evt.keyCode == 27;
         }
         if (isEnter) {
           // Get the target from the "data-target" attribute
@@ -56,8 +56,8 @@ var callback = function(){
           var $target = document.getElementById(target);
 
           // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.add('is-active');
-          $target.classList.add('is-active');
+          $el.classList.add("is-active");
+          $target.classList.add("is-active");
         }
 
         if (isEscape) {
@@ -66,8 +66,8 @@ var callback = function(){
           var $target = document.getElementById(target);
 
           // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.remove('is-active');
-          $target.classList.remove('is-active');
+          $el.classList.remove("is-active");
+          $target.classList.remove("is-active");
         }
       };
     });
@@ -76,34 +76,34 @@ var callback = function(){
   // =======================================
   // Load comments when it's in the viewport
   // =======================================
-  var comments = document.getElementById('comments');
+  var comments = document.getElementById("comments");
 
   if (comments) {
-    var scroll = function(e) {
-      if(isInViewport(comments) == true) {
+    var scroll = function (e) {
+      if (isInViewport(comments) == true) {
         loadComments();
-        document.removeEventListener('scroll', scroll, true);
+        document.removeEventListener("scroll", scroll, true);
       }
     };
-    document.addEventListener('scroll', scroll, true);
+    document.addEventListener("scroll", scroll, true);
   }
 
   // =============
   // Image Gallery
   // =============
-  var images = document.querySelectorAll('.kg-gallery-image img');
+  var images = document.querySelectorAll(".kg-gallery-image img");
   images.forEach(function (image) {
-      var container = image.closest('.kg-gallery-image');
-      var width = image.attributes.width.value;
-      var height = image.attributes.height.value;
-      var ratio = width / height;
-      container.style.flex = ratio + ' 1 0%';
-  })
+    var container = image.closest(".kg-gallery-image");
+    var width = image.attributes.width.value;
+    var height = image.attributes.height.value;
+    var ratio = width / height;
+    container.style.flex = ratio + " 1 0%";
+  });
 };
 
 if (
-    document.readyState === "complete" ||
-    (document.readyState !== "loading" && !document.documentElement.doScroll)
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
 ) {
   callback();
 } else {
@@ -119,17 +119,17 @@ function isInViewport(el) {
   var width = el.offsetWidth;
   var height = el.offsetHeight;
 
-  while(el.offsetParent) {
+  while (el.offsetParent) {
     el = el.offsetParent;
     top += el.offsetTop;
     left += el.offsetLeft;
   }
 
   return (
-    top < (window.pageYOffset + window.innerHeight) &&
-    left < (window.pageXOffset + window.innerWidth) &&
-    (top + height) > window.pageYOffset &&
-    (left + width) > window.pageXOffset
+    top < window.pageYOffset + window.innerHeight &&
+    left < window.pageXOffset + window.innerWidth &&
+    top + height > window.pageYOffset &&
+    left + width > window.pageXOffset
   );
 }
 
@@ -142,14 +142,14 @@ function newLazyLoad() {
     class_loading: "loading",
     class_loaded: "loaded",
     treshold: 100,
-    callback_enter: function(el) {
+    callback_enter: function (el) {
       // addClass('.lazyload', 'loading');
-      el.classList.add('loading');
+      el.classList.add("loading");
     },
-    callback_set: function(el) {
-      el.classList.remove('loading');
-      el.classList.add('loaded');
-    }
+    callback_set: function (el) {
+      el.classList.remove("loading");
+      el.classList.add("loaded");
+    },
   });
 }
 
@@ -165,7 +165,7 @@ function addClass(selector, myClass) {
   elements = document.querySelectorAll(selector);
 
   // add class to all chosen elements
-  for (var i=0; i<elements.length; i++) {
+  for (var i = 0; i < elements.length; i++) {
     elements[i].classList.add(myClass);
   }
 }
@@ -178,7 +178,7 @@ function removeClass(selector, myClass) {
   elements = document.querySelectorAll(selector);
 
   // remove class from all chosen elements
-  for (var i=0; i<elements.length; i++) {
+  for (var i = 0; i < elements.length; i++) {
     elements[i].classList.remove(myClass);
   }
 }
@@ -188,18 +188,23 @@ function removeClass(selector, myClass) {
 // =============
 function loadComments() {
   /**
-  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-  */
-  var disqus_config = function () {
-    this.page.url = '{{url absolute="true"}}';  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = '{{comment_id}}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+   *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+   *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+   */
+
+  var DiscourseEmbed = {
+    discourseUrl: "https://forum.kmd.io/",
+    discourseEmbedUrl: '{{ url absolute = "true" }}',
   };
 
-  (function () {  // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
+  (function () {
+    var d = document.createElement("script");
+    d.type = "text/javascript";
+    d.async = true;
+    d.src = DiscourseEmbed.discourseUrl + "javascripts/embed.js";
+    (
+      document.getElementsByTagName("head")[0] ||
+      document.getElementsByTagName("body")[0]
+    ).appendChild(d);
   })();
 }
